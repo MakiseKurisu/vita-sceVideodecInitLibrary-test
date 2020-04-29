744814957f6585ee49f7070291f60c16557438f9
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2020-04-29 14:32:54 -0500

    Test resolution from 1x1 to 2560x2560

--- src/main.c
+++ src/main.c
@@ -1,17 +1,63 @@
 #include <psp2/kernel/threadmgr.h>
 #include <psp2/kernel/processmgr.h>
+#include <psp2/videodec.h> 
 #include <stdio.h>
 
 #include "debugScreen.h"
-
 
 #define printf psvDebugScreenPrintf
 
 int main(int argc, char *argv[]) {
 	psvDebugScreenInit();
-	printf("Hello, world!\n");
-	
+
+	FILE* log = fopen("ux0:data/sceVideodecInitLibrary.log", "w");
+	if (log == NULL)
+	{
+		printf("Failed to open log file.\n");
+		sceKernelDelayThread(3*1000000);
+
+		sceKernelExitProcess(0);
+		return 1;
+	}
+
+	printf("Resolution test begin in 3 seconds.\n");
+	sceKernelDelayThread(3*1000000);
+
+	SceVideodecQueryInitInfoHwAvcdec init;
+	init.size = sizeof(SceVideodecQueryInitInfoHwAvcdec);
+	init.numOfRefFrames = 5;
+    init.numOfStreams = 1;
+
+	for (int x = 1; x <= 2560; x++)
+	{
+		printf("x = %i\n", x);
+		init.horizontal = x;
+		for (int y = 1; y <= 2560; y++)
+		{
+			init.vertical = y;
+			if (!sceVideodecInitLibrary(SCE_VIDEODEC_TYPE_HW_AVCDEC, &init))
+			{
+      			fprintf(log, "%i, %i\n", x, y);
+    			if (sceVideodecTermLibrary(SCE_VIDEODEC_TYPE_HW_AVCDEC) < 0)
+				{
+					fflush(log);
+
+					printf("Cannot release decoder.\n");
+					sceKernelDelayThread(3*1000000);
+
+					sceKernelExitProcess(0);
+					return 2;
+				}
+			}
+		}
+	}
+
+	fflush(log);
+	fclose(log);
+
+	printf("Test completed.\n");
 	sceKernelDelayThread(3*1000000); // Wait for 3 seconds
+
 	sceKernelExitProcess(0);
 	return 0;
 }
